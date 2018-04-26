4aa3931d689ba684749ecabc0793280e6a653af1
Luke Rodgers <lukerodgers90@gmail.com>
2018-04-26 09:46:20 +0100

    add icu4c@55.1

---
+++ Formula/icu4c@55.1.rb
@@ -0,0 +1,28 @@
+class Icu4cAT551 < Formula
+  homepage "http://site.icu-project.org/"
+  head "https://ssl.icu-project.org/repos/icu/icu/trunk/", :using => :svn
+  url "https://ssl.icu-project.org/files/icu4c/55.1/icu4c-55_1-src.tgz"
+  version "55.1"
+  sha256 "e16b22cbefdd354bec114541f7849a12f8fc2015320ca5282ee4fd787571457b"
+
+  option :universal
+  option :cxx11
+
+  def install
+    ENV.universal_binary if build.universal?
+    ENV.cxx11 if build.cxx11?
+
+    args = ["--prefix=#{prefix}", "--disable-samples", "--disable-tests", "--enable-static"]
+    args << "--with-library-bits=64" if MacOS.prefer_64_bit?
+    cd "source" do
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/gendict", "--uchars", "/usr/share/dict/words", "dict"
+  end
+
+end
