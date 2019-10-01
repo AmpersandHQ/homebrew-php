3e07a47ca7ba788b306e1833ce5a8bdc09b0f9dc
Luke Rodgers <lr@amp.co>
2019-10-01 14:31:12 +0100

    add mirror for icu4c@55.1

--- Formula/icu4c@55.1.rb
+++ Formula/icu4c@55.1.rb
@@ -2,6 +2,7 @@ class Icu4cAT551 < Formula
   homepage "http://site.icu-project.org/"
   head "https://ssl.icu-project.org/repos/icu/icu/trunk/", :using => :svn
   url "https://ssl.icu-project.org/files/icu4c/55.1/icu4c-55_1-src.tgz"
+  mirror "https://github.com/AmpersandHQ/homebrew-php/raw/master/files/icu4c-55_1-src.tgz"
   version "55.1"
   sha256 "e16b22cbefdd354bec114541f7849a12f8fc2015320ca5282ee4fd787571457b"
 
