8750e6cc2197c2f88bc4c0d2423d79d5f944a231
Luke Rodgers <lr@amp.co>
2019-10-07 15:28:27 +0100

    Add yarn@1.5.1

---
+++ Formula/yarn@1.5.1.rb
@@ -0,0 +1,29 @@
+class YarnAT151 < Formula
+  desc "JavaScript package manager"
+  homepage "https://yarnpkg.com/"
+  # Should only be updated if the new version is listed as a stable release on the homepage
+  url "https://yarnpkg.com/downloads/1.5.1/yarn-v1.5.1.tar.gz"
+  mirror "https://github.com/AmpersandHQ/homebrew-php/raw/master/files/yarn-v1.5.1.tar.gz"
+  sha256 "cd31657232cf48d57fdbff55f38bfa058d2fb4950450bd34af72dac796af4de1"
+
+  keg_only :versioned_formula
+
+  bottle :unneeded
+
+  depends_on "node"
+
+  conflicts_with "hadoop", :because => "both install `yarn` binaries"
+
+  def install
+    libexec.install Dir["*"]
+    (bin/"yarn").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
+    (bin/"yarnpkg").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
+    inreplace "#{libexec}/package.json", '"installationMethod": "tar"', '"installationMethod": "homebrew"'
+  end
+
+  test do
+    (testpath/"package.json").write('{"name": "test"}')
+    system bin/"yarn", "add", "jquery"
+    system bin/"yarn", "add", "fsevents", "--build-from-source=true"
+  end
+end
