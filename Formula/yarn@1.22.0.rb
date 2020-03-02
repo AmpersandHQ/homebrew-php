1540413698caa501fb5dfec5bfa22da74f64b57f
Luke Rodgers <lr@amp.co>
2020-03-02 11:12:21 +0000

    Add yarn@1.22.0

---
+++ Formula/yarn@1.22.0.rb
@@ -0,0 +1,26 @@
+class YarnAT1220 < Formula
+  desc "JavaScript package manager"
+  homepage "https://yarnpkg.com/"
+  # Should only be updated if the new version is listed as a stable release on the homepage
+  url "https://yarnpkg.com/downloads/1.22.0/yarn-v1.22.0.tar.gz"
+  sha256 "de8871c4e2822cba80d58c2e72366fb78567ec56e873493c9ca0cca76c60f9a5"
+
+  keg_only :versioned_formula
+
+  depends_on "node"
+
+  conflicts_with "hadoop", :because => "both install `yarn` binaries"
+
+  def install
+    libexec.install Dir["*"]
+    (bin/"yarn").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
+    (bin/"yarnpkg").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
+    inreplace "#{libexec}/package.json", '"installationMethod": "tar"', '"installationMethod": "homebrew"'
+  end
+
+  test do
+    (testpath/"package.json").write('{"name": "test"}')
+    system bin/"yarn", "add", "jquery"
+    system bin/"yarn", "add", "fsevents", "--build-from-source=true"
+  end
+end
