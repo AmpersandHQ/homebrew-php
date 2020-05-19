2020d6078aa9212e8ea7bf16ff66d814aef6796c
Luke Rodgers <lr@amp.co>
2020-05-19 10:24:29 +0100

    Remove erroneously defined bottles.
    These formula have not been bottled and hosted on bintray

--- Formula/amp-php@5.5-mongo.rb
+++ Formula/amp-php@5.5-mongo.rb
@@ -8,13 +8,6 @@ class AmpPhpAT55Mongo < AbstractPhp55Extension
   sha256 "586a0f55d29198010da5f4c932a183491f114db6e1b0ba8e40e7246b1a4a96d0"
   head "https://github.com/mongodb/mongo-php-driver-legacy.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "028bed8264633fb6e140f275650f4891d77e0d0108110b3157ce46ced8d95e61" => :el_capitan
-    sha256 "30b661227951f09507bfb2eaa370e1933cfa140d2682c97a387542dfcf3f5e61" => :yosemite
-    sha256 "9e9fc0871ab9bf8f3cb5f24497cb5017c26a809c70f88333d0e5479b2ffc5c56" => :mavericks
-  end
-
   def install
     Dir.chdir "mongo-#{version}" unless build.head?
 
