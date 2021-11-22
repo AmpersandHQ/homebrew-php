b8a1bd47932d1a77f1db478959c59cd9c66c6e36
Luke Rodgers <lr@amp.co>
2021-11-22 16:56:43 +0000

    Increase php-fpm processes

--- Formula/amp-php@5.6.rb
+++ Formula/amp-php@5.6.rb
@@ -211,6 +211,10 @@ class AmpPhpAT56 < Formula
     # Increase default max_execution_time as magento in developer mode is slow
     system "sed -i '' 's/max_execution_time = 30/max_execution_time = 240/' #{etc}/php/#{php_version}/php.ini"
 
+    # Set fpm to static mode with 10 processes
+    system "sed -i '' 's/pm = dynamic/pm = static/' #{etc}/php/#{php_version}/php-fpm.d/www.conf"
+    system "sed -i '' 's/pm.max_children = 5/pm.max_children = 10/' #{etc}/php/#{php_version}/php-fpm.d/www.conf"
+
     # Ensure opcache is disabled
     system "rm -f #{etc}/php/#{php_version}/conf.d/ext-opcache.ini"
 
--- Formula/amp-php@8.0.rb
+++ Formula/amp-php@8.0.rb
@@ -228,6 +228,10 @@ class AmpPhpAT80 < Formula
     system "sed -i '' 's/max_execution_time = 30/max_execution_time = 240/' #{etc}/php/#{php_version}/php.ini"
     system "sed -i '' 's/max_execution_time = 60/max_execution_time = 240/' #{etc}/php/#{php_version}/php.ini"
 
+    # Set fpm to static mode with 10 processes
+    system "sed -i '' 's/pm = dynamic/pm = static/' #{etc}/php/#{php_version}/php-fpm.d/www.conf"
+    system "sed -i '' 's/pm.max_children = 5/pm.max_children = 10/' #{etc}/php/#{php_version}/php-fpm.d/www.conf"
+
     # Be doubly sure that opcache.ini file is not generated for local usage
     system "rm -f #{etc}/php/#{php_version}/conf.d/ext-opcache.ini"
 
