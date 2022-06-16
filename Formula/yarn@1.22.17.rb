316fdf102195465fe73f6c6bdc7b836021ed9d10
Luke Rodgers <lr@amp.co>
2022-06-16 09:51:17 +0100

    Add yarn@1.22.17

---
+++ Formula/yarn@1.22.17.rb
@@ -0,0 +1,26 @@
+class YarnAT12217 < Formula
+  desc "JavaScript package manager"
+  homepage "https://yarnpkg.com/"
+  # Should only be updated if the new version is listed as a stable release on the homepage
+  url "https://yarnpkg.com/downloads/1.22.17/yarn-v1.22.17.tar.gz"
+  sha256 "267982c61119a055ba2b23d9cf90b02d3d16c202c03cb0c3a53b9633eae37249"
+
+  keg_only :versioned_formula
+
+  depends_on "node"
+
+  conflicts_with "hadoop", :because => "both install `yarn` binaries"
+
+  def install
+    libexec.install Dir["*"]
+    (bin/"yarn").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
+    (bin/"yarnpkg").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
+    inreplace "#{libexec}/package.json", '"installationMethod": "tar"', '"installationMethod": "homebrew"'
+  end
+
+  test do
+    (testpath/"package.json").write('{"name": "test"}')
+    system bin/"yarn", "add", "jquery"
+    system bin/"yarn", "add", "fsevents", "--build-from-source=true"
+  end
+end
